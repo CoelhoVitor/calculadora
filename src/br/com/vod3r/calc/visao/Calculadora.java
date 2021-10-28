a04289a1673583efbe61c68bcc8e8396450a305a
CoelhoVitor <vitorvcs@hotmail.com>
2021-10-28 17:14:31 -0300

    primeiras aulas

---
+++ src/br/com/vod3r/calc/visao/Calculadora.java
@@ -0,0 +1,33 @@
+package br.com.vod3r.calc.visao;
+
+import java.awt.*;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Calculadora extends JFrame {
+
+	public Calculadora() {
+		
+		organizarLayout();
+		
+		setSize(232, 322);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	private void organizarLayout() {
+		setLayout(new BorderLayout());
+		
+		Display display = new Display();
+		add(display, BorderLayout.NORTH);
+		
+		Teclado teclado = new Teclado();
+		add(teclado, BorderLayout.CENTER);
+	}
+
+	public static void main(String[] args) {
+		new Calculadora();
+	}
+}
